67a54b27391589cc0ca722c517da747513192f20
Rishi Bhardwaj <rishi2208bhardwaj@gmail.com>
2026-07-30 23:48:10 +0530

    Serve static files from 'dist' directory

--- server.ts
+++ server.ts
@@ -10,6 +10,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json({ limit: '50mb' }));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Helper to get GenAI client
